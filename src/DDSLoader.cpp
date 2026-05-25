340ba3ecfb008b160890e718839c6b23f170c3a6
Ash Matheson <ash.matheson.dev@gmail.com>
2026-05-24 17:48:03 -0700

    feat: add DDSLoader interface stub

--- src/DDSLoader.cpp
+++ src/DDSLoader.cpp
@@ -1,1 +1,5 @@
-// placeholder
+#include "DDSLoader.h"
+
+std::expected<TextureData, std::string> DDSLoader::Load(const std::filesystem::path&) {
+    return std::unexpected(std::string("DDSLoader not yet implemented"));
+}
--- src/DDSLoader.h
+++ src/DDSLoader.h
@@ -1,1 +1,10 @@
 #pragma once
+#include <expected>
+#include <filesystem>
+#include <string>
+#include "TextureData.h"
+
+class DDSLoader {
+public:
+    static std::expected<TextureData, std::string> Load(const std::filesystem::path& path);
+};
